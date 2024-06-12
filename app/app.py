e42ea85374406d41bbe59803df3133d4f617421b
hitesh009911 <90308743+hitesh009911@users.noreply.github.com>
2024-06-12 16:07:20 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,22 +13,19 @@ import logging
 app = Flask(__name__)
 
 # Azure Blob Storage configuration
-storage_account_name = "vini1"
-storage_account_key = "EmHFiicyGw1J6F1zar/GCmHHX/2mIABfthMsbC6PxVGGyXM2RmBahl8EwUfL9qPKncku/QSDc37x+ASt/FvieA=="
-container_name = "images"
+storage_account_name = "YOUR_STORAGE_ACCOUNT_NAME"
+storage_account_key = "YOUR_STORAGE_ACCOUNT_KEY"
+container_name = "YOUR_CONTAINER_NAME"
 
 # Azure Cosmos DB configuration
-cosmosdb_endpoint = "https://vini.documents.azure.com:443/"
-cosmosdb_key = "2qnj1CvfQF7Zi69d85x6PaYoJpZWQI9DOs2K1kNPxc0QF2Pk7umgHRJX0U3D5ZnGb3WzdK1Mnz2BACDbBaDQ2Q=="
-cosmosdb_database_name = "image"
-cosmosdb_container_name = "cont1"
+cosmosdb_endpoint = "YOUR_COSMOS_DB_ENDPOINT"
+cosmosdb_key = "YOUR_COSMOS_DB_KEY"
+cosmosdb_database_name = "YOUR_DATABASE_NAME"
+cosmosdb_container_name = "YOUR_CONTAINER_NAME"
 
 # Azure Cognitive Services configuration
-cog_endpoint = "https://vinicon.cognitiveservices.azure.com/"
-cog_key = "8a6343bfbd334aa8b183ae8e3b924354"
-
-face_key = '8fe6cd0d048346f88d8f261b8859e74a'
-face_endpoint = 'https://python-face.cognitiveservices.azure.com/'
+cog_endpoint = "https://your_cog_endpoint.cognitiveservices.azure.com/"
+cog_key = "your_cog_key"
 
 # Initialize Blob Service Client
 blob_service_client = BlobServiceClient.from_connection_string(
